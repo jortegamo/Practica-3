15a22e1bedaf1b012c8a67d26b43c2c8d15c7a99
Jorge Ortega Morata <j.ortega.morata@gmail.com>
2014-10-22 17:38:54 +0200

    Añadidos algunos tests iniciales para la clase PlayerMissile

--- 08-missiles/spec/PlayerMissileSpec.js
+++ 08-missiles/spec/PlayerMissileSpec.js
@@ -30,16 +30,39 @@
 
 describe ("Clase PlayerMissile",function(){
 
+	beforeEach (function(){
+		loadFixtures('index.html');
+		SpriteSheet.load (sprites,function(){});
+		oldGame = Game;
+	});
+	
+	afterEach (function(){
+		Game = oldGame;
+	});
+	
 	it("Definida la clase",function(){
 		expect(PlayerMissile).toBeDefined();
 	});
 	
 	it("Creando un misil",function(){
-	
+		Game = {width: 320, height: 480};
+		var missile = new PlayerMissile(Game.width/2,Game.height);
+		expect(missile.x).toBe(159);
+		expect(missile.y).toBe(470);
+		expect(missile.w).toBe(2);
+		expect(missile.h).toBe(10);
+		expect(missile.vy).toBe(-700);
 	});
 	
 	it("step",function(){
-	
+		//creamos el board al que se van a a–adir los misiles.
+		var board = new GameBoard();
+		Game = {width: 320, height: 480};
+		var missile1 = new PlayerMissile(Game.width/2, Game.height); //estara en la pos (159,470);
+		board.add(missile1);
+		spyOn(missile1,"step");
+		board.step(1);
+		expect(missile1.step).toHaveBeenCalledWith(30);
 	});
 	
 	it("draw",function(){
